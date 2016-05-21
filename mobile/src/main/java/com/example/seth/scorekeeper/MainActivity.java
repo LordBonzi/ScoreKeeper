5a88a97f874c533e599a28835a8eede530aa541f
sds1612 <seth.d.schroeder@gmail.com>
2016-05-21 16:39:11 +0100

    deleted fab

--- mobile/src/main/java/com/example/seth/scorekeeper/MainActivity.java
+++ mobile/src/main/java/com/example/seth/scorekeeper/MainActivity.java
@@ -4,9 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -74,19 +72,6 @@ public class MainActivity extends AppCompatActivity
         }else{
             normal.setVisibility(View.VISIBLE);
 
-        }
-
-
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        if (fab != null) {
-            fab.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                            .setAction("Action", null).show();
-                }
-            });
         }
 
         //navigation drawer stuff

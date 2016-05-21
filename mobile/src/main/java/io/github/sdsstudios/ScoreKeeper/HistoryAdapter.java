06113c43e34c2e323b2ba5c8c371c46d04cde7bd
sds1612 <seth.d.schroeder@gmail.com>
2016-05-21 22:51:34 +0100

    changed applicationId to io.github.sdsstudios.ScoreKeeper

---
+++ mobile/src/main/java/io/github/sdsstudios/ScoreKeeper/HistoryAdapter.java
@@ -0,0 +1,69 @@
+package io.github.sdsstudios.ScoreKeeper;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by seth on 08/05/16.
+ */
+public class HistoryAdapter extends RecyclerView.Adapter<HistoryAdapter.ViewHolder> {
+    private ArrayList<String> mDataset;
+
+    // Provide a reference to the views for each data item
+    // Complex data items may need more than one view per item, and
+    // you provide access to all the views for a data item in a view holder
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        // each data item is just a string in this case
+        public TextView textViewWinner;
+        public TextView textViewLoser;
+        public TextView textViewDate;
+
+        public ViewHolder(View v) {
+            super(v);
+            textViewWinner = (TextView)v.findViewById(R.id.textViewWinner);
+            textViewLoser = (TextView)v.findViewById(R.id.textViewLoser);
+            textViewDate = (TextView)v.findViewById(R.id.textViewDate);
+
+        }
+    }
+
+    // Provide a suitable constructor (depends on the kind of dataset)
+    public HistoryAdapter(ArrayList<String> myDataset) {
+        mDataset = myDataset;
+    }
+
+    // Create new views (invoked by the layout manager)
+    @Override
+    public HistoryAdapter.ViewHolder onCreateViewHolder(ViewGroup parent,
+                                                        int viewType) {
+        // create a new view
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.history_adapter, parent, false);
+        // set the view's size, margins, paddings and layout parameters
+
+        ViewHolder vh = new ViewHolder(view);
+        return vh;
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        // - get element from your dataset at this position
+        // - replace the contents of the view with that element
+        holder.textViewWinner.setText(mDataset.get(0));
+        holder.textViewLoser.setText(mDataset.get(1));
+        holder.textViewDate.setText(mDataset.get(1));
+
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return mDataset.size();
+    }
+}
---
+++ mobile/src/main/java/io/github/sdsstudios/ScoreKeeper/MainActivity.java
@@ -0,0 +1,253 @@
+package io.github.sdsstudios.ScoreKeeper;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener, View.OnClickListener {
+
+    String TAG = "MainActivity.class";
+    public static Integer P1Score =0 , P2Score =0;
+    int gameSize;
+    public static int gameID;
+    RelativeLayout normal, big;
+    ArrayList playersArray;
+    public static ArrayList scoresArray;
+    CursorHelper cursorHelper;
+    public static Button buttonP1;
+    public static Button buttonP2;
+    SmallLayout smallLayout;
+    BigLayout bigLayout;
+
+    private ScoreDBAdapter dbHelper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        Log.e("MainActivity", "Started mainactivity");
+
+        buttonP1 = (Button) findViewById(R.id.buttonP1);
+        buttonP1.setOnClickListener(this);
+
+        buttonP2 = (Button) findViewById(R.id.buttonP2);
+        buttonP2.setOnClickListener(this);
+        normal = (RelativeLayout)findViewById(R.id.layoutNormal);
+        big = (RelativeLayout)findViewById(R.id.layoutBig);
+
+        cursorHelper = new CursorHelper();
+
+        dbHelper = new ScoreDBAdapter(this);
+        dbHelper.open();
+
+        scoresArray = new ArrayList();
+        scoresArray = cursorHelper.getDBCursorArray(ScoreDBAdapter.KEY_SCORE, dbHelper);
+
+        smallLayout = new SmallLayout();
+        smallLayout.onCreate();
+        bigLayout = new BigLayout();
+        bigLayout.onCreate();
+
+        gameID = Integer.valueOf(dbHelper.getNewestGame());
+
+        gameSize = cursorHelper.getDBCursorArray(ScoreDBAdapter.KEY_PLAYERS, dbHelper).size();
+
+        playersArray= new ArrayList();
+        playersArray = cursorHelper.getDBCursorArray(ScoreDBAdapter.KEY_PLAYERS, dbHelper);
+
+
+
+        if (gameSize > 2) {
+            big.setVisibility(View.VISIBLE);
+        }else{
+            normal.setVisibility(View.VISIBLE);
+
+        }
+
+
+
+        //navigation drawer stuff
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        //Shared Preferences stuff
+        final String PREFS_NAME = "scorekeeper";
+
+        SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+
+        if (settings.getBoolean("my_first_time", true)) {
+
+            saveInfo();
+            settings.edit().putBoolean("my_first_time", false).commit();
+        }else {
+            SharedPreferences sharedPref = getSharedPreferences("scorekeeper"
+                    , Context.MODE_PRIVATE);
+
+        }
+
+    }
+
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.buttonP1:
+                    smallLayout.game(buttonP1, P1Score, dbHelper, scoresArray, gameID);
+
+                break;
+
+            case R.id.buttonP2:
+                smallLayout.game(buttonP2, P2Score, dbHelper, scoresArray, gameID);
+
+                break;
+        }
+    }
+
+    public void saveInfo(){
+        SharedPreferences sharedPref = getSharedPreferences("scorekeeper", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+
+
+        editor.apply();
+    }
+
+    @Override
+    public void onBackPressed() {
+        moveTaskToBack(true);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_history) {
+            Intent intent = new Intent(this, History.class);
+            startActivity(intent);
+
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+
+}
+
+class BigLayout extends  Activity{
+
+    public void onCreate(){
+    }
+
+    public void game(Button button){
+
+
+
+    }
+}
+
+class SmallLayout extends Activity{
+
+    public void onCreate(){
+        updateScores();
+        MainActivity.buttonP1.setText("0");
+        MainActivity.buttonP2.setText("0");
+    }
+
+    public void game(Button button, Integer score, ScoreDBAdapter dbHelper, ArrayList arrayList, int id){
+        score += 1;
+        button.setText(String.valueOf(score));
+        dbHelper.updateGame(arrayList, ScoreDBAdapter.KEY_SCORE, id);
+        updateScores();
+    }
+
+    public void updateScores(){
+        if (MainActivity.P1Score == null || MainActivity.P2Score == null){
+            MainActivity.P1Score = 0;
+            MainActivity.scoresArray.add(0, String.valueOf(MainActivity.P1Score));
+            MainActivity.P2Score = 0;
+            MainActivity.scoresArray.add(1, String.valueOf(MainActivity.P2Score));
+
+        }else{
+
+            MainActivity.P1Score = Integer.valueOf(MainActivity.scoresArray.get(0).toString());
+            MainActivity.P2Score = Integer.valueOf(MainActivity.scoresArray.get(1).toString());
+
+
+        }
+    }
+
+}

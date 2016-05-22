0dd0343307cdfe476da404777416068ba33b8e09
sds1612 <seth.d.schroeder@gmail.com>
2016-05-22 22:06:48 +0100

    open github repo on button click

--- mobile/src/main/java/io/github/sdsstudios/ScoreKeeper/About.java
+++ mobile/src/main/java/io/github/sdsstudios/ScoreKeeper/About.java
@@ -3,24 +3,10 @@ package io.github.sdsstudios.ScoreKeeper;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.text.TextUtils;
-import android.util.Log;
-import android.view.KeyEvent;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-
-import java.util.ArrayList;
 
 public class About extends AppCompatActivity
         implements View.OnClickListener {
@@ -43,6 +29,9 @@ public class About extends AppCompatActivity
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.buttonInfo:
+                startActivity(url);
+
+            break;
 
 
         }

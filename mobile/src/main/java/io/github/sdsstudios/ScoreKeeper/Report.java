cd390cc988228e2ea58d4f19c382eb105a40dba4
sds1612 <seth.d.schroeder@gmail.com>
2016-06-13 19:31:47 +0100

    added report and stepper

---
+++ mobile/src/main/java/io/github/sdsstudios/ScoreKeeper/Report.java
@@ -0,0 +1,42 @@
+package io.github.sdsstudios.ScoreKeeper;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.crash.FirebaseCrash;
+
+public class Report extends AppCompatActivity implements View.OnClickListener{
+    private Button buttonReportAction;
+    private EditText editTextReport;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_report);
+        editTextReport = (EditText)findViewById(R.id.editTextReport);
+        buttonReportAction = (Button)findViewById(R.id.buttonReportAction);
+        buttonReportAction.setOnClickListener(this);
+
+        Thread.setDefaultUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
+
+            @Override
+            public void uncaughtException(Thread t, Throwable e) {
+                FirebaseCrash.report(new Exception(t.toString()));
+
+            }
+        });
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        FirebaseCrash.report(new Exception(editTextReport.getText().toString()));
+        Toast toast = Toast.makeText(this, "Sent Report", Toast.LENGTH_SHORT);
+
+
+    }
+}

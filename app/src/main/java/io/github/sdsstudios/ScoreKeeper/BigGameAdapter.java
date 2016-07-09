f18e54651441f5bc11727c99846eeff824213dd4
sds1612 <seth.d.schroeder@gmail.com>
2016-07-09 21:38:21 +0100

    test

---
+++ app/src/main/java/io/github/sdsstudios/ScoreKeeper/BigGameAdapter.java
@@ -0,0 +1,129 @@
+package io.github.sdsstudios.ScoreKeeper;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by seth on 08/05/16.
+ */
+public class BigGameAdapter extends RecyclerView.Adapter<BigGameAdapter.ViewHolder> {
+    ScoreDBAdapter dbHelper;
+    ArrayList arrayListScore;
+    int gameID;
+    private ArrayList<BigGameModel> mBigGameModel;
+    private boolean enabled;
+
+
+    // Provide a suitable constructor (depends on the kind of dataset)
+    public BigGameAdapter(ArrayList<BigGameModel> myDataset, ArrayList score, ScoreDBAdapter dbAdapter, int id, boolean menabled) {
+        mBigGameModel = myDataset;
+        dbHelper =dbAdapter;
+        arrayListScore = score;
+        gameID = id;
+        enabled = menabled;
+    }
+
+    // Create new views (invoked by the layout manager)
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent,
+                                                        int viewType) {
+        // create a new view
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.big_game_adapter, parent, false);
+        // set the view's size, margins, paddings and layout parameters
+
+        ViewHolder vh = new ViewHolder(view);
+        return vh;
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(final ViewHolder holder, final int position) {
+        // - get element from your dataset at this position
+        // - replace the contents of the view with that element
+        final BigGameModel bigGameModel = mBigGameModel.get(position);
+
+        holder.textViewPlayer.setText(bigGameModel.getPlayers());
+        holder.butonScore.setText(bigGameModel.getScore());
+
+        if (enabled) {
+
+            holder.butonScore.setOnClickListener(new View.OnClickListener() {
+
+                @Override
+                public void onClick(View v) {
+                    int score = 0;
+                    int buttonScore = 0;
+
+                    buttonScore = Integer.valueOf(holder.butonScore.getText().toString());
+                    score = buttonScore += 1;
+                    holder.butonScore.setText(String.valueOf(score));
+                    arrayListScore.set(position, String.valueOf(score));
+                    dbHelper.open();
+                    dbHelper.updateGame(arrayListScore, null, ScoreDBAdapter.KEY_SCORE, gameID);
+                    dbHelper.close();
+
+                }
+            });
+
+            holder.butonScore.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View v) {
+                    int score = 0;
+                    int buttonScore = 0;
+
+                    buttonScore = Integer.valueOf(holder.butonScore.getText().toString());
+                    score = buttonScore -= 1;
+
+                    if (score == -1) {
+
+                    } else {
+                        holder.butonScore.setText(String.valueOf(score));
+                        arrayListScore.set(position, String.valueOf(score));
+                        dbHelper.open();
+                        dbHelper.updateGame(arrayListScore, null, ScoreDBAdapter.KEY_SCORE, gameID);
+                        dbHelper.close();
+                    }
+
+                    return true;
+                }
+            });
+
+        }else{
+            holder.butonScore.setEnabled(false);
+        }
+    }
+
+    public void closeDB(){
+        dbHelper.close();
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return mBigGameModel.size();
+    }
+
+    // Provide a reference to the views for each data item
+    // Complex data items may need more than one view per item, and
+    // you provide access to all the views for a data item in a view holder
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        // each data item is just a string in this case
+        public TextView textViewPlayer;
+        public Button butonScore;
+
+
+        public ViewHolder(View v) {
+            super(v);
+            textViewPlayer = (TextView)v.findViewById(R.id.listTextViewPlayer);
+            butonScore = (Button) v.findViewById(R.id.listButtonScore);
+
+        }
+    }
+}
---
+++ app/src/main/java/io/github/sdsstudios/ScoreKeeper/Home.java
@@ -0,0 +1,247 @@
+package io.github.sdsstudios.ScoreKeeper;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.RelativeLayout;
+
+import com.google.firebase.analytics.FirebaseAnalytics;
+
+public class Home extends AppCompatActivity implements UpdateTabsListener{
+
+    private Intent newGameIntent;
+    private Intent settingsIntent;
+    private Intent aboutIntent;
+    private RecyclerView.Adapter adapter;
+    private ScoreDBAdapter dbHelper;
+    private RelativeLayout relativeLayout;
+    private FirebaseAnalytics mFirebaseAnalytics;
+
+
+
+    /**
+     * The {@link android.support.v4.view.PagerAdapter} that will provide
+     * fragments for each of the sections. We use a
+     * {@link FragmentPagerAdapter} derivative, which will keep every
+     * loaded fragment in memory. If this becomes too memory intensive, it
+     * may be best to switch to a
+     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
+     */
+    private SectionsPagerAdapter mSectionsPagerAdapter;
+
+    /**
+     * The {@link ViewPager} that will host the section contents.
+     */
+    private CustomViewPager mViewPager;
+    private MenuItem deleteMenuItem, settingsMenuItem;
+    private Toolbar toolbar;
+    private TabLayout tabLayout;
+    private int currentTab = 3;
+    AppBarLayout appBarLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        tabLayout = (TabLayout) findViewById(R.id.tabs);
+        appBarLayout = (AppBarLayout)findViewById(R.id.appbar);
+
+        // Create the adapter that will return a fragment for each of the three
+        // primary sections of the activity.
+        dbHelper = new ScoreDBAdapter(this).open();
+        mViewPager = (CustomViewPager) findViewById(R.id.container);
+
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+
+
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+        tabLayout.setupWithViewPager(mViewPager);
+        mViewPager.setPagingEnabled(true);
+
+        mViewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                currentTab = position;
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+
+
+
+
+
+        newGameIntent = new Intent(this, NewGame.class);
+        aboutIntent = new Intent(this, About.class);
+        settingsIntent = new Intent(this, Settings.class);
+        relativeLayout = (RelativeLayout) findViewById(R.id.historyLayout);
+
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fabNewGame);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+                startActivity(newGameIntent);
+            }
+        });
+
+
+    }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        deleteMenuItem = menu.findItem(R.id.action_delete);
+        settingsMenuItem = menu.findItem(R.id.action_settings);
+        deleteMenuItem.setVisible(false);
+
+        return true;
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        dbHelper.open();
+
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        dbHelper.close();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            startActivity(settingsIntent);
+            return true;
+        }if (id == R.id.action_about) {
+            startActivity(aboutIntent);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+
+    }
+
+    @Override
+    public void gamesDeleted() {
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+        // Set up the ViewPager with the sections adapter.
+        mViewPager = (CustomViewPager) findViewById(R.id.container);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+        tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(mViewPager);
+        mViewPager.setCurrentItem(this.currentTab);
+
+    }
+
+    @Override
+    public void multiSelectEnabled() {
+        AppBarLayout.LayoutParams layoutParams = new AppBarLayout.LayoutParams(0,0);
+        tabLayout.setLayoutParams(layoutParams);
+        toolbar.setLayoutParams(layoutParams);
+        mViewPager.setPagingEnabled(false);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+
+            getWindow().setStatusBarColor(getResources().getColor(R.color.colorPrimaryDark));
+        }
+
+    }
+
+    @Override
+    public void multiSelectDisabled() {
+        AppBarLayout.LayoutParams layoutParams = new AppBarLayout.LayoutParams(AppBarLayout.LayoutParams.MATCH_PARENT, AppBarLayout.LayoutParams.WRAP_CONTENT);
+        tabLayout.setLayoutParams(layoutParams);
+        toolbar.setLayoutParams(layoutParams);
+        mViewPager.setPagingEnabled(true);
+    }
+
+    /**
+     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
+     * one of the sections/tabs/pages.
+     */
+    public class SectionsPagerAdapter extends FragmentPagerAdapter{
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            // getItem is called to instantiate the fragment for the given page.
+            // Return a PlaceholderFragment (defined as a static inner class below).
+
+            return PlaceholderFragment.newInstance(position + 1);
+
+        }
+
+        @Override
+        public int getCount() {
+            // Show 3 total pages.
+            return 3;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            switch (position) {
+                case 0:
+                    return getResources().getString(R.string.in_progress);
+                case 1:
+                    return getResources().getString(R.string.completed);
+                case 2:
+                    return getResources().getString(R.string.all_games);
+
+            }
+            return null;
+        }
+    }
+}
+
+
+

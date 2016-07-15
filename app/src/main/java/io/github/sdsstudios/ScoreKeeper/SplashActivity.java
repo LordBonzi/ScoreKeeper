661d4d3413bf3f2c152f176f85e122e9cd658443
sds1612 <seth.d.schroeder@gmail.com>
2016-07-15 22:22:31 +0100

    added basic implementation of splash screen

---
+++ app/src/main/java/io/github/sdsstudios/ScoreKeeper/SplashActivity.java
@@ -0,0 +1,24 @@
+package io.github.sdsstudios.ScoreKeeper;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by seth on 15/07/16.
+ */
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this, Home.class);
+        startActivity(intent);
+        finish();
+
+
+
+    }
+
+}
